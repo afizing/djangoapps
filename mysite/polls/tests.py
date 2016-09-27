b730d81869835d93462f97d847c664f179760724
Afiz <afizing@gmail.com>
2016-09-27 12:56:41 +0530

    test cases are updated for List/Details views

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 import datetime
 
 from .models import Question
-from .views
+
 # Create your tests here.
 
 class QuestionMethodTests(TestCase):
@@ -22,3 +23,30 @@ class QuestionMethodTests(TestCase):
     time = timezone.now()- datetime.timedelta(days=30)
     f_question = Question(pub_date=time)
     self.assertEqual(f_question.was_published_recently(), False)
+
+
+def create_question(question_text, days):
+  time = timezone.now() + timezone.timedelta(days=days)
+  return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+  def test_index_view_with_no_questions(self):
+    response = self.client.get(reverse('polls:index'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "No polls are available.")
+    self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+  def test_index_view_with_a_past_questions(self):
+    create_question(question_text="Past question.", days=-30)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(response.context['latest_question_list'],
+    ['<Question: Past question.>'])
+
+
+class QuestionIndexDetailTests(TestCase):
+  def test_detail_view_with_a_future_question(self):
+    future_question = create_question(question_text='Future question.', days=5)
+    url = reverse('polls:detail', args=(future_question.id,))
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 404)
+
---
+++ mysite/readme.txt
@@ -0,0 +1,25 @@
+
+>>> Testing the application 
+
+++ Run the below command from your project root direcotry(where manage.py presents)
+
+C:\Afiz\GitApps\djangoapps\mysite>python manage.py test polls
+
+++ You should be getting results if everything goes well. 
+
+Creating test database for alias 'default'...
+.....
+----------------------------------------------------------------------
+Ran 5 tests in 0.267s
+
+OK
+Destroying test database for alias 'default'...
+
+C:\Afiz\GitApps\djangoapps\mysite>python manage.py test polls
+Creating test database for alias 'default'...
+......
+----------------------------------------------------------------------
+Ran 6 tests in 1.176s
+
+OK
+Destroying test database for alias 'default'...
