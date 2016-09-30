da37af2f62f74e5fa20be6fdf64873ded5ebe377
Afiz <afizing@gmail.com>
2016-09-30 17:05:28 +0530

    list-create-update-delete views are added for blog

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 url(r'^$', views.index, name='index'),
 url(r'^(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
 url(r'^create/$', views.create, name='create'),
-
-
+url(r'^(?P<post_id>[0-9]+)/update/$', views.update, name='update'),
+url(r'^(?P<post_id>[0-9]+)/delete/$', views.delete, name='delete'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
 def index(request):
-  posts = Post.objects.all()
+  posts = Post.objects.all().order_by('timestamp')
   context = {
-  'posts':posts
+  'posts':posts.reverse()
   }
   return render(request, 'blog/index.html', context)
 
@@ -25,10 +27,32 @@ def create(request):
   if form.is_valid():
     instance = form.save(commit=False)
     instance.save()
+    messages.success(request, "New post is created")
+    return HttpResponseRedirect(instance.get_absolute_url())
+
   context = {
     'form':form
   }
   return render(request, 'blog/create.html', context)
+
+
+def update(request, post_id=None):
+  instance = get_object_or_404(Post, id=post_id)
+  form = PostForm(request.POST or None, instance=instance)
+  if form.is_valid():
+    instance = form.save(commit=False)
+    instance.save()
+    messages.success(request, "Post Updated")
+    return HttpResponseRedirect(instance.get_absolute_url())
+    # redirect('blog:detail', id=post_id)
+  return render(request, 'blog/update.html', {'form': form})
+
+
+def delete(request, post_id=None):
+  instance = get_object_or_404(Post, id=post_id)
+  instance.delete()
+  messages.success(request, "Post deleted")
+  return redirect('blog:index')
 
 
 def hello(request):
